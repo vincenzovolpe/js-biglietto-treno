d7d9f901e21ce561f4ac44ef168428f4617155dd
vincenzovolpe <vinci792010@gmail.com>
2019-11-14 18:10:20 +0100

    Aggiustati commenti e messaggi a schermo

--- main.js
+++ main.js
@@ -19,31 +19,24 @@ var costobiglietto40 = (costobigliettointero - (costobigliettointero * (sconto40
 
 
 // Controllo km inseriti ed richiesta e controllo età del cliente e stampa relativo messaggio di costo del biglietto
-if (isNaN(kmetri)) {
-    //alert("Il valore inserito non è numerico");
+if (isNaN(kmetri)) { // Controllo se km inserito è un numero
     document.getElementById('notnumber').setAttribute("class", "visibile");
-} else if (! (kmetri > 0 )) {
-    //alert("Il valore inserito è un numero negativo");
+} else if (! (kmetri > 0 )) { // Controllo se km inserito è > 0
     document.getElementById('notpositive').setAttribute("class", "visibile");
 } else {
     var eta = prompt('Quanti anni hai?'); // richiesta età dell'utente
-    if (isNaN(eta)) {
-        //alert("Il valore inserito non è numerico");
+    if (isNaN(eta)) { // Controllo se l'età inserita è un numero
         document.getElementById('notnumber').setAttribute("class", "visibile");
-    } else if (! (eta > 0 )) {
-        //alert("Il valore inserito è un numero negativo");
+    } else if (! (eta > 0 )) { // Controllo se l'età inserita è > 0
         document.getElementById('notpositive').setAttribute("class", "visibile");
     } else {
         if (eta >= 18 && eta <= 65) {
-            //alert("Il costo del tuo biglietto è di Euro: " + costobigliettointero);
             document.getElementById('messaggio').setAttribute("class", "visibile");
             document.getElementById('costobiglietto').innerHTML = costobigliettointero;
         } else if (eta < 18){
-            //alert("Il costo del tuo biglietto è di Euro (scontato del 20%): " + costobiglietto20);
             document.getElementById('messaggio').setAttribute("class", "visibile");
             document.getElementById('costobiglietto').innerHTML = costobiglietto20;
         } else {
-            //alert("Il costo del tuo biglietto è di Euro (scontato del 40%): " + costobiglietto40);
             document.getElementById('messaggio').setAttribute("class", "visibile");
             document.getElementById('costobiglietto').innerHTML = costobiglietto40;
         }
