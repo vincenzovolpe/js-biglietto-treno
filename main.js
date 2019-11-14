ff01d33fff01bd78b929b4a347e4b54439498acd
vincenzovolpe <vinci792010@gmail.com>
2019-11-14 16:28:46 +0100

    Inserito controllo se il valore inserito è numerico e positivo

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // Richiesta km da percorrere e  età all'utente
 var kmetri = prompt('Quanti chilometri vuoi percorrere?');
-console.log(kmetri);
+
+
+
 var eta = prompt('Quanti anni hai?');
-console.log(eta);
+
 // Costo del biglietto al km
 const costoperkm = 0.21;
 
@@ -12,21 +14,26 @@ const sconto40 = 40; //sconto 40% per pensionati (> 65 anni)
 
 // Calcolo del costo del biglietto intero
 var costobigliettointero = kmetri * costoperkm;
-console.log(costobigliettointero);
+
 
 // Calcolo del costo del biglietto scontato del 20%
 var costobiglietto20 = (costobigliettointero - (costobigliettointero * (sconto20 / 100)));
-console.log(costobiglietto20);
 
 // Calcolo del costo del biglietto scontato del 40%
 var costobiglietto40 = (costobigliettointero - (costobigliettointero * (sconto40 / 100)));
-console.log(costobiglietto40);
+
 
 // Controllo età del cliente e relativo messaggio di costo del biglietto
-if (eta >= 18 && eta <= 65) {
-    alert("Il costo del tuo biglietto è di Euro: " + costobigliettointero);
-} else if (eta < 18){
-    alert("Il costo del tuo biglietto è di Euro: " + costobiglietto20);
+if (isNaN(eta)) {
+    alert("Il valore inserito non è numerico");
+} else if (! (eta > 0 )) {
+    alert("Il valore inserito è un numero negativo");
 } else {
-    alert("Il costo del tuo biglietto è di Euro: " + costobiglietto40);
+    if (eta >= 18 && eta <= 65) {
+        alert("Il costo del tuo biglietto è di Euro: " + costobigliettointero);
+    } else if (eta < 18){
+        alert("Il costo del tuo biglietto è di Euro: " + costobiglietto20);
+    } else {
+        alert("Il costo del tuo biglietto è di Euro: " + costobiglietto40);
+    }
 }
