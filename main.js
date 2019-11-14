5997561d46d6c4fe09deda4a9a5d6400063925f6
vincenzovolpe <vinci792010@gmail.com>
2019-11-14 18:00:53 +0100

    Esercizio inserito in hmtl con foglio di stile

--- main.js
+++ main.js
@@ -21,28 +21,31 @@ var costobiglietto40 = (costobigliettointero - (costobigliettointero * (sconto40
 // Controllo km inseriti ed richiesta e controllo età del cliente e stampa relativo messaggio di costo del biglietto
 if (isNaN(kmetri)) {
     //alert("Il valore inserito non è numerico");
-    document.writeln("Il valore inserito non è numerico")
+    document.getElementById('notnumber').setAttribute("class", "visibile");
 } else if (! (kmetri > 0 )) {
     //alert("Il valore inserito è un numero negativo");
-    document.writeln("Il valore inserito è un numero negativo")
+    document.getElementById('notpositive').setAttribute("class", "visibile");
 } else {
     var eta = prompt('Quanti anni hai?'); // richiesta età dell'utente
     if (isNaN(eta)) {
         //alert("Il valore inserito non è numerico");
-        document.writeln("Il valore inserito non è numerico")
+        document.getElementById('notnumber').setAttribute("class", "visibile");
     } else if (! (eta > 0 )) {
         //alert("Il valore inserito è un numero negativo");
-        document.writeln("Il valore inserito è un numero negativo")
+        document.getElementById('notpositive').setAttribute("class", "visibile");
     } else {
         if (eta >= 18 && eta <= 65) {
             //alert("Il costo del tuo biglietto è di Euro: " + costobigliettointero);
-            document.writeln("Il costo del tuo biglietto è di Euro: " + costobigliettointero);
+            document.getElementById('messaggio').setAttribute("class", "visibile");
+            document.getElementById('costobiglietto').innerHTML = costobigliettointero;
         } else if (eta < 18){
             //alert("Il costo del tuo biglietto è di Euro (scontato del 20%): " + costobiglietto20);
-            document.writeln("Il costo del tuo biglietto è di Euro: " + costobiglietto20);
+            document.getElementById('messaggio').setAttribute("class", "visibile");
+            document.getElementById('costobiglietto').innerHTML = costobiglietto20;
         } else {
             //alert("Il costo del tuo biglietto è di Euro (scontato del 40%): " + costobiglietto40);
-            document.writeln("Il costo del tuo biglietto è di Euro: " + costobiglietto40);
+            document.getElementById('messaggio').setAttribute("class", "visibile");
+            document.getElementById('costobiglietto').innerHTML = costobiglietto40;
         }
     }
 }
