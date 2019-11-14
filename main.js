d557fbfd20094f19651a360f7bf7bab1283747c4
vincenzovolpe <vinci792010@gmail.com>
2019-11-14 17:17:22 +0100

    Aggiustati commenti al codice javascript e ordine delle istruzioni

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
-// Richiesta km da percorrere e  età all'utente
-var kmetri = prompt('Quanti chilometri vuoi percorrere?');
-
 // Costo del biglietto al km
 const costoperkm = 0.21;
 
-// Percentuali di sconto
+// Percentuali di sconto dei biglietti
 const sconto20 = 20; //sconto 20% per minorenni (< 18 anni)
 const sconto40 = 40; //sconto 40% per pensionati (> 65 anni)
 
+// Richiesta km da percorrere
+var kmetri = prompt('Quanti chilometri vuoi percorrere?');
+
 // Calcolo del costo del biglietto intero
 var costobigliettointero = Math.round((kmetri * costoperkm));
-
 
 // Calcolo del costo del biglietto scontato del 20%
 var costobiglietto20 = Math.round((costobigliettointero - (costobigliettointero * (sconto20 / 100))));
@@ -19,13 +18,13 @@ var costobiglietto20 = Math.round((costobigliettointero - (costobigliettointero 
 var costobiglietto40 = Math.round((costobigliettointero - (costobigliettointero * (sconto40 / 100))));
 
 
-// Controllo km inseriti ed età del cliente e stampa relativo messaggio di costo del biglietto
+// Controllo km inseriti ed richiesta e controllo età del cliente e stampa relativo messaggio di costo del biglietto
 if (isNaN(kmetri)) {
     alert("Il valore inserito non è numerico");
 } else if (! (kmetri > 0 )) {
     alert("Il valore inserito è un numero negativo");
 } else {
-    var eta = prompt('Quanti anni hai?');
+    var eta = prompt('Quanti anni hai?'); // richiesta età dell'utente
     if (isNaN(eta)) {
         alert("Il valore inserito non è numerico");
     } else if (! (eta > 0 )) {
